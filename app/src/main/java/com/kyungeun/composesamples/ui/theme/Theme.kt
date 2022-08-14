e4e414223046b551c8620accee8652f0993887df
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-08-14 23:42:40 +0900

    Image : Add drawable images

--- app/src/main/java/com/kyungeun/composesamples/ui/theme/Theme.kt
+++ app/src/main/java/com/kyungeun/composesamples/ui/theme/Theme.kt
@@ -2,30 +2,71 @@ package com.kyungeun.composesamples.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    surface = Color.Blue,
-    onSurface = Color.DarkGray,
-    primary = Color.LightGray,
-    onPrimary = Color.DarkGray
-)
+//private val DarkColorPalette = darkColors(
+//    surface = Color.Blue,
+//    onSurface = Color.DarkGray,
+//    primary = Color.LightGray,
+//    onPrimary = Color.DarkGray
+//)
+//
+//private val LightColorPalette = lightColors(
+//    surface = Color.Blue,
+//    onSurface = Color.White,
+//    primary = Color.LightGray,
+//    onPrimary = Color.DarkGray
+//)
+//
+//@Composable
+//fun ComposeSamplesTheme(
+//    darkTheme: Boolean = isSystemInDarkTheme(),
+//    content: @Composable () -> Unit
+//) {
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
+//
+//    MaterialTheme(
+//        colors = colors,
+//        typography = Typography,
+//        shapes = Shapes,
+//        content = content
+//    )
+//}
+
 
 private val LightColorPalette = lightColors(
-    surface = Color.Blue,
-    onSurface = Color.White,
-    primary = Color.LightGray,
-    onPrimary = Color.DarkGray
+    primary = gray900,
+    secondary = rust600,
+    background = taupe100,
+    surface = Color.White.copy(alpha = .85f),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = taupe800,
+    onSurface = gray900.copy(alpha = 0.8f)
+)
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    secondary = rust300,
+    background = gray900,
+    surface = Color.White.copy(alpha = 0.15f),
+    onPrimary = gray900,
+    onSecondary = gray900,
+    onBackground = taupe100,
+    onSurface = Color.White.copy(alpha = .8f)
 )
 
 @Composable
-fun ComposeSamplesTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun ComposeSamplesTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
@@ -34,8 +75,8 @@ fun ComposeSamplesTheme(
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
-        shapes = Shapes,
+        typography = typography,
+        shapes = shapes,
         content = content
     )
 }
--- app/src/main/java/com/kyungeun/composesamples/ui/theme/Type.kt
+++ app/src/main/java/com/kyungeun/composesamples/ui/theme/Type.kt
@@ -5,24 +5,86 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.kyungeun.composesamples.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+//// Set of Material typography styles to start with
+//val Typography = Typography(
+//    body1 = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp
+//    )
+//    /* Other default text styles to override
+//    button = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.W500,
+//        fontSize = 14.sp
+//    ),
+//    caption = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 12.sp
+//    )
+//    */
+//)
+
+
+
+private val fontFamilyKulim = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.kulim_park_regular
+        ),
+        Font(
+            resId = R.font.kulim_park_light,
+            weight = FontWeight.Light
+        )
     )
-    /* Other default text styles to override
+)
+
+private val fontFamilyLato = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.lato_regular
+        ),
+        Font(
+            resId = R.font.lato_bold,
+            weight = FontWeight.Bold
+        )
+    )
+)
+
+val typography = Typography(
+    defaultFontFamily = fontFamilyLato,
+    h1 = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontWeight = FontWeight.Light,
+        fontSize = 28.sp,
+        letterSpacing = (1.15).sp
+    ),
+    h2 = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontSize = 15.sp,
+        letterSpacing = (1.15).sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    body1 = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = (1.15).sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontFamily = fontFamilyKulim,
+        fontSize = 12.sp,
+        letterSpacing = (1.15).sp
+    ),
 )
